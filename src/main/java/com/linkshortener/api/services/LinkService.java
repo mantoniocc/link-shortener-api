a4075df0c9adec5ea26900cd6d56f1e08a9adb2e
Mario Cortés <marioantonio.cortes@gmail.com>
2025-09-25 12:33:22 -0300

    Update LinkService with repository

--- src/main/java/com/linkshortener/api/services/LinkService.java
+++ src/main/java/com/linkshortener/api/services/LinkService.java
@@ -3,15 +3,29 @@ package com.linkshortener.api.services;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import com.linkshortener.api.domain.Link;
+import com.linkshortener.api.repositories.LinkRepository;
+
 @Service
 public class LinkService {
+
+    private final LinkRepository linkRepository;
+
+    // Constructor Injection for the repository
+    public LinkService(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
     
     public String createShortLink(String longUrl) {
         // Generate a random 7-character alphanumeric string
         String shortCode = RandomStringUtils.randomAlphanumeric(7);
 
-        // We'll save this to the database later.
-        // For now, we'll just return it.
+        // Create a new Link entity
+        Link newLink = new Link(longUrl, shortCode);
+
+        // Save the new entity to the database
+        linkRepository.save(newLink);
+
         return shortCode;
     }
 }
