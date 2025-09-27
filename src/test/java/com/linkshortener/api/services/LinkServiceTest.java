ca59633d94653a5967484d865bf541b5f487ba16
Mario Cortés <marioantonio.cortes@gmail.com>
2025-09-27 11:25:32 -0300

    Add test

---
+++ src/test/java/com/linkshortener/api/services/LinkServiceTest.java
@@ -0,0 +1,35 @@
+package com.linkshortener.api.services;
+
+import com.linkshortener.api.repositories.LinkRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import com.linkshortener.api.repositories.LinkRepository;
+
+@ExtendWith(MockitoExtension.class)
+class LinkServiceTest {
+    
+    @Mock
+    private LinkRepository  linkRepository;
+
+    @InjectMocks
+    private LinkService linkService;
+
+    @Test
+    void createShortLink_shouldReturn7CharacteString() {
+        // Given (Arrange)
+        String longUrl = "https://www.example.com";
+
+        // When (Act)
+        String shortCode = linkService.createShortLink(longUrl);
+
+        // Then (Assert)
+        assertNotNull(shortCode);
+        assertEquals(7, shortCode.length());
+    }
+}
